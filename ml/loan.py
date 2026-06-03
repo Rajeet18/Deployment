10bf72a92516b4f97338713c1acc62ea4c13c97d
patel rajeet <patelrajeet01@gmail.com>
2026-06-03 10:30:35 +0530

    commit
    commit

--- ml/loan.py
+++ ml/loan.py
@@ -7,7 +7,7 @@ model=joblib.load("Loan_approval_model.joblib")
 
 @app.get("/")
 def home():
-    return {"message": "loan REJECT"}
+    return {"message": "loan approval for me"}
 
 # cd API
 #  python -m uvicorn loan:app --reload
