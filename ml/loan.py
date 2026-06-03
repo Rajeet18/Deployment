37697c0acbd46a536d5db52b531fcaf9587c794d
patel rajeet <patelrajeet01@gmail.com>
2026-06-03 10:21:45 +0530

    commit
    commit

--- ml/loan.py
+++ ml/loan.py
@@ -3,11 +3,11 @@ import joblib
 
 app = FastAPI()
 
-model=joblib.load("diabetes_model.joblib")
+model=joblib.load("Loan_approval_model.joblib")
 
 @app.get("/")
 def home():
-    return {"message": "loan API is Running"}
+    return {"message": "loan REJECT"}
 
 # cd API
 #  python -m uvicorn loan:app --reload
